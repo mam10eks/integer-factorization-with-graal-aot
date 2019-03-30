957837e1c4aa3179f36f52393898fb3b72fe484d
Maik Fröbe <mam10eks@studserv.uni-leipzig.de>
2019-03-30 09:35:19 +0100

    Created maven archetype

---
+++ src/main/java/com/empty_stack/ifwga/App.java
@@ -0,0 +1,13 @@
+package com.empty_stack.ifwga;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
