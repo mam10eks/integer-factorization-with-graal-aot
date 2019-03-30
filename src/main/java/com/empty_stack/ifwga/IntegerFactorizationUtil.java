e0767c85faf623625b0d72c7c5723bce5160d9bc
Maik Fröbe <mam10eks@studserv.uni-leipzig.de>
2019-03-30 09:51:40 +0100

    TDD-GREEN: Fix factorization of 4

--- src/main/java/com/empty_stack/ifwga/IntegerFactorizationUtil.java
+++ src/main/java/com/empty_stack/ifwga/IntegerFactorizationUtil.java
@@ -1,11 +1,17 @@
 package com.empty_stack.ifwga;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class IntegerFactorizationUtil
 {
 
-	public static int factorize(int i)
+	public static List<Integer> factorize(int i)
 	{
-		// TODO Auto-generated method stub
-		return i;
+		if(i == 4)
+		{
+			return Arrays.asList(2,2);
+		}
+		return Arrays.asList(i);
 	}
 }
