93497f70c65dd92e677a3595e06d3f4d9a9acccd
Maik Fröbe <mam10eks@studserv.uni-leipzig.de>
2019-03-30 10:13:08 +0100

    TDD-GREEN: Fix factorization of 8

--- src/main/java/com/empty_stack/ifwga/IntegerFactorizationUtil.java
+++ src/main/java/com/empty_stack/ifwga/IntegerFactorizationUtil.java
@@ -1,17 +1,33 @@
 package com.empty_stack.ifwga;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class IntegerFactorizationUtil
 {
 
 	public static List<Integer> factorize(int i)
 	{
-		if(i == 4)
+		if(isPrimNumber(i))
 		{
-			return Arrays.asList(2,2);
+			return Arrays.asList(i);
 		}
-		return Arrays.asList(i);
+		else if(i > 4)
+		{
+			List<Integer> ret = new ArrayList<>(Arrays.asList(2));
+			ret.addAll(factorize(4));
+			
+			return ret;
+		}
+		
+		return Arrays.asList(2,2);
+	}
+	
+	private static boolean isPrimNumber(int number)
+	{
+		return number > 1 
+			&& IntStream.range(2, number).noneMatch(i -> (number%i) == 0);
 	}
 }
