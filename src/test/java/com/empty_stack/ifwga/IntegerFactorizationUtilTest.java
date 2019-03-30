2e26ef4f6f9e5d90bdcb884b0e9b73e395a66961
Maik Fröbe <mam10eks@studserv.uni-leipzig.de>
2019-03-30 09:50:36 +0100

    TDD-RED: failing test to calculate factorization of 4

--- src/test/java/com/empty_stack/ifwga/IntegerFactorizationUtilTest.java
+++ src/test/java/com/empty_stack/ifwga/IntegerFactorizationUtilTest.java
@@ -1,5 +1,8 @@
 package com.empty_stack.ifwga;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -8,17 +11,26 @@ public class IntegerFactorizationUtilTest
 	@Test
 	public void checkThatFactorizationOf2Returns2()
 	{
-		int expected = 2;
-		int actual = IntegerFactorizationUtil.factorize(2);
+		List<Integer> expected = Arrays.asList(2);
+		List<Integer> actual = IntegerFactorizationUtil.factorize(2);
 		
+		Assert.assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void checkThatFactorizarionof4Returns2_2()
+	{
+		List<Integer> expected = Arrays.asList(2, 2);
+		List<Integer> actual = IntegerFactorizationUtil.factorize(4);
+
 		Assert.assertEquals(expected, actual);
 	}
 	
 	@Test
 	public void checkThatFactorizationOf5Returns5()
 	{
-		int expected = 5;
-		int actual = IntegerFactorizationUtil.factorize(5);
+		List<Integer> expected = Arrays.asList(5);
+		List<Integer> actual = IntegerFactorizationUtil.factorize(5);
 
 		Assert.assertEquals(expected, actual);
 	}
